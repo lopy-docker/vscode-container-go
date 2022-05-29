492878007d49f0e5cc9809169d99f1f1f0d990f3
zy@home <284141050@qq.com>
2022-05-29 21:21:10 +0800

    feat(all): add go grpc

--- sh_go.sh
+++ sh_go.sh
@@ -12,6 +12,7 @@ go install github.com/go-delve/delve/cmd/dlv@latest
 go install honnef.co/go/tools/cmd/staticcheck@latest
 go install golang.org/x/tools/gopls@latest
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
 echo "echo general tools"
 go install github.com/lopygo/about/cmd/aboutbuilder@latest
