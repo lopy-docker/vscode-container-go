7856c9115dde06692b49e6f3927dbc0ce212989b
zy@home <284141050@qq.com>
2022-06-03 14:12:54 +0800

    feat(all): add docker

--- sh_sys.sh
+++ sh_sys.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 apt update 
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+
+add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) \
+    stable"
+
+apt-get update
+
 apt install -y upx g++-mingw-w64-x86-64 g++-mingw-w64-i686 \
     bash-completion \
     protobuf-compiler \
+    docker-ce-cli \
     clang-format
 
 # 
