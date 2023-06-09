b3d3c3cd57b45888a9e7416de3d8f0b6a9071abf
Zy on debian11 <284141050@qq.com>
2023-06-09 16:08:21 +0800

    feat(all): add changelog-cli

--- sh_npm.sh
+++ sh_npm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /usr/local/share/nvm/nvm.sh  
-npm install -g commitizen cz-conventional-changelog
+npm install -g commitizen cz-conventional-changelog conventional-changelog-cli
 echo '{ "path": "cz-conventional-changelog" }' > ~/.czrc
