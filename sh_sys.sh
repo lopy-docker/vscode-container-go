70ecbee29e1d467bcd6f07abe63ea9cbe8ca8994
zengyu <284141050@qq.com>
2025-04-25 23:01:44 +0800

    update docker installer

--- sh_sys.sh
+++ sh_sys.sh
@@ -7,19 +7,24 @@ apt-get install \
     gnupg2 \
     software-properties-common -y
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 
-add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/debian \
-    $(lsb_release -cs) \
-    stable"
+# install docker
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt-get update
+apt update
 
+apt install -y docker-ce-cli docker-buildx-plugin docker-compose-plugin
+
+# gcc...
 apt install -y upx g++-mingw-w64-x86-64 g++-mingw-w64-i686 \
     bash-completion \
     protobuf-compiler \
-    docker-ce-cli \
     clang-format
 
 
