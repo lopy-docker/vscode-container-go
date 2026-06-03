e7ba60c3865b4a68fcafa73db262fb42ebe3b6f9
zengyu <284141050@qq.com>
2026-06-04 02:18:08 +0800

    test go script

--- sh_go.sh
+++ sh_go.sh
@@ -12,9 +12,40 @@ go install github.com/go-delve/delve/cmd/dlv@latest
 go install honnef.co/go/tools/cmd/staticcheck@latest
 go install golang.org/x/tools/gopls@latest
 
+# Detect Go version to install compatible tools
+GO_VERSION_FULL=$(go version | awk '{print $3}' | sed 's/go//')
+GO_VERSION_MAJOR=$(echo $GO_VERSION_FULL | cut -d. -f1)
+GO_VERSION_MINOR=$(echo $GO_VERSION_FULL | cut -d. -f2)
+
+# Default versions (latest)
+PROTOC_GEN_GO_VERSION="latest"
+PROTOC_GEN_GO_GRPC_VERSION="latest"
+GOLANGCI_LINT_VERSION="v1.64.7"
+
+# Pin versions for compatibility with older Go versions
+if [ "$GO_VERSION_MAJOR" -eq 1 ]; then
+    if [ "$GO_VERSION_MINOR" -le 20 ]; then
+        PROTOC_GEN_GO_VERSION="v1.33.0"
+        PROTOC_GEN_GO_GRPC_VERSION="v1.3.0"
+        GOLANGCI_LINT_VERSION="v1.53.3"
+    elif [ "$GO_VERSION_MINOR" -le 22 ]; then
+        # v1.34.0+ requires Go 1.21+
+        # v1.60.0+ for golangci-lint requires Go 1.22+
+        PROTOC_GEN_GO_VERSION="v1.34.2"
+        PROTOC_GEN_GO_GRPC_VERSION="v1.5.1"
+        if [ "$GO_VERSION_MINOR" -eq 21 ]; then
+            GOLANGCI_LINT_VERSION="v1.55.2"
+        else
+            GOLANGCI_LINT_VERSION="v1.60.2"
+        fi
+    fi
+fi
+
+echo "Detected Go $GO_VERSION_FULL: using protoc-gen-go@$PROTOC_GEN_GO_VERSION, protoc-gen-go-grpc@$PROTOC_GEN_GO_GRPC_VERSION, golangci-lint@$GOLANGCI_LINT_VERSION"
+
 echo "install go grpc cli"
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+go install google.golang.org/protobuf/cmd/protoc-gen-go@${PROTOC_GEN_GO_VERSION}
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@${PROTOC_GEN_GO_GRPC_VERSION}
 
 echo "echo general tools"
 go install github.com/lopygo/about/cmd/aboutbuilder@latest
@@ -24,7 +55,7 @@ go install golang.org/x/tools/cmd/stringer@latest
 go install github.com/spf13/cobra-cli@latest
 
 # 
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.64.7
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s -- -b $(go env GOPATH)/bin ${GOLANGCI_LINT_VERSION}
 golangci-lint --version
 
 # go get github.com/go-bindata/go-bindata/...
