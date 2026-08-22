ccc3a6fd7751ec2b517687260bb937b5db4b8c21
zengyu <284141050@qq.com>
2026-08-23 01:43:44 +0800

    go 1.23

--- scripts/sh_go/1.23.sh
+++ scripts/sh_go/1.23.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "install go grpc cli for 1.23"
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.36.12
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.5.1
 go install golang.org/x/tools/cmd/stringer@v0.34.0
 
 echo "install golangci-lint v1.64.7"
